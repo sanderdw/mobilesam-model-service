b6ebb4502591715a00b6889734f195952a1dd4dc
Sander de Wildt <sanderdw@gmail.com>
2025-06-02 13:36:47 +0200

    use_container_width

--- frontend/app.py
+++ frontend/app.py
@@ -27,7 +27,7 @@ if is_api_up is True:
     uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
     if uploaded_file is not None:
         image = Image.open(uploaded_file)
-        st.image(image, caption="Uploaded Image", use_column_width=True)
+        st.image(image, caption="Uploaded Image", use_container_width=True)
 
         try:
             files = {"file": uploaded_file.getvalue()}
@@ -36,7 +36,7 @@ if is_api_up is True:
             if response.status_code == 200:
                 segmented_image = Image.open(BytesIO(response.content))
                 st.image(
-                    segmented_image, caption="Segmented Image", use_column_width=True
+                    segmented_image, caption="Segmented Image", use_container_width=True
                 )
             else:
                 st.error("Failed to segment the provided image")
